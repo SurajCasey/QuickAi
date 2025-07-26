f0e4c8cc5dcf0e4c6f545f372005440d365be743
Suraj Khatri <84454285+SurajCasey@users.noreply.github.com>
2025-07-26 21:49:41 +1000

    server folder added

---
+++ user/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Tesimonial from '../components/Testimonials'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>  
+    <Hero/>
+    <AiTools/>
+    <Tesimonial/>
+    <Plan/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
